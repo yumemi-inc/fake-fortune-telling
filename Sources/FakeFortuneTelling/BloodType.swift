0227c6068bddabd591e5dad653345a58df4cf2c1
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-08-16 23:57:39 +0900

    Add types for input

---
+++ Sources/FakeFortuneTelling/BloodType.swift
@@ -0,0 +1,17 @@
+//
+//  BloodType.swift
+//  
+//
+//  Created by 史 翔新 on 2022/08/16.
+//
+
+import Foundation
+
+public enum BloodType: Decodable {
+    
+    case o
+    case a
+    case b
+    case ab
+    
+}
---
+++ Sources/FakeFortuneTelling/YearMonthDay.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by 史 翔新 on 2022/08/16.
+//
+
+import Foundation
+
+public struct YearMonthDay {
+    
+    public enum Error: Swift.Error {
+        case invalidDate
+    }
+    
+    let year: Int
+    let month: Int
+    let day: Int
+    
+    init(year: Int, month: Int, day: Int) throws {
+        
+        let dateComponents = DateComponents(calendar: .init(identifier: .gregorian), year: year, month: month, day: day)
+        guard dateComponents.isValidDate else {
+            throw Error.invalidDate
+        }
+        
+        self.year = year
+        self.month = month
+        self.day = day
+        
+    }
+    
+}
